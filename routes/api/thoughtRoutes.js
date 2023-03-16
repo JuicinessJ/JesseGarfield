723d17e378f3641bbf0fb568f9c566f8aa47a0d4
Jesse <JuicinessJMN@Gmail.com>
2023-03-16 15:39:51 -0500

    added routes in for thoughts, still need to verify if controllers were done correctly, still awaiting dayjs

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const {} = require('../../controllers/thoughtController')
+const { getThoughts, getSingleThought, createThought, updateThought, deleteThought, createReaction, deleteReaction } = require('../../controllers/thoughtController')
 
-router.route('/').get(get)
+router.route('/').get(getThoughts).get(getSingleThought).post(createThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
 
 module.exports = router;
